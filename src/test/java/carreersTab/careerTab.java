14b12aa177f7c38297d4a1a1fbdbdcdffad00e96
Mirna Viquez <mirna.viquez@gorillalogic.com>
2017-07-12 09:57:30 -0600

    Step def

---
+++ src/test/java/carreersTab/careerTab.java
@@ -0,0 +1,29 @@
+package carreersTab;
+
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+import cucumber.runtime.PendingException;
+
+/**
+ * Created by mirna on 7/12/17.
+ */
+public class careerTab {
+    @Given("^I am on Gorilla Page$")
+    public void iAmOnGorillaPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I click on Careers tab$")
+    public void iClickOnCareersTab() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Career page loads$")
+    public void careerPageLoads() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
