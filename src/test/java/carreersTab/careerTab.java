65198e57f4a69eb930a8dd034a7b425352d6023c
Mirna Viquez <mirna.viquez@gorillalogic.com>
2017-07-12 10:23:06 -0600

    Test

--- src/test/java/carreersTab/careerTab.java
+++ src/test/java/carreersTab/careerTab.java
@@ -4,24 +4,27 @@ import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
 import cucumber.runtime.PendingException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utilities.browserSetup;
 
 /**
  * Created by mirna on 7/12/17.
  */
 public class careerTab {
-    @Given("^I am on Gorilla Page$")
+    @Given("^I am on the Gorilla Logic homepage$")
     public void iAmOnGorillaPage() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
-    @When("^I click on Careers tab$")
+    @When("^I click on Careers Tab$")
     public void iClickOnCareersTab() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
-    @Then("^Career page loads$")
+    @Then("^Careers page is showed up$")
     public void careerPageLoads() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
